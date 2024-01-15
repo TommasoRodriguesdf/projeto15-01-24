9487ea2a605571dc7076b025bd9a3b9edf9c1d26
TommasoRodriguesdf <dtommasorodrigues@gmail.com>
2024-01-15 16:08:38 -0400

    aula1601

--- bmw.js
+++ bmw.js
@@ -1,13 +1,14 @@
 const listaProdutos = [{
     id: 1,
     nome: "cavalo",
-    preco: 10.10,
+    preco: 10.11,
+    imagen:"https://cdn.autopapo.com.br/box/uploads/2021/10/20131814/motor-v8-big-block-chevrolet-10.4-632.jpg",
 },
 {
-    id: 1,
+    id: 2,
     nome: "cavala",
-    preco: 10.10,
-
+    preco: 10.11,
+    imagen:"https://d1gl66oyi6i593.cloudfront.net/wp-content/uploads/2018/03/Motor-V5.jpg",
 }
 ];
 let main = document.querySelector("main");
@@ -16,10 +17,25 @@ console.log(main);
 
 listaProdutos.map(
    (item)=>{
+      let div = document.createElement("div");
+      main.appendChild(div);
+
       let p = document.createElement("p");
       main.appendChild(p);
-
+      div.appendChild(p);
       p.textContent = item.nome;
+
+      let span = document.createElement("span");
+      main.appendChild(span);
+      div.appendChild(span);
+      span.textContent = item.preco
+
+      let img = document.createElement("img");
+      main.appendChild(img);
+      div.appendChild(img);
+      img.src = item.imagen;
+
+    
    }
 
 );
